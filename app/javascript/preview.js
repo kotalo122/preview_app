403a36a6d1a226fdd9852bf1977d3d4254e6b62d
kotalo122 <kotalo122@gmail.com>
2023-10-03 14:58:45 +0900

    input要素で値の変化が起きた際に呼び出

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -9,4 +9,13 @@ document.addEventListener('DOMContentLoaded', function(){
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!postForm) return null;
   console.log("preview.jsが読み込まれました");
+
+  // input要素を取得　　　<div class="click-upload">の中にあるinput要素　　
+  // 検証を参照<input type="file" name="post[image]" id="post_image">
+  const fileField = document.querySelector('input[type="file"][name="post[image]"]');
+
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', function(e){
+    console.log("input要素で値の変化がありました");
+  });
 });
