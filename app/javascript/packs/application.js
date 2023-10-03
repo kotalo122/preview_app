161f67b9f7749002ad79d8462c6ccaaa4e06bbf0
kotalo122 <kotalo122@gmail.com>
2023-10-03 13:09:30 +0900

    application.jsの編集

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,12 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
+// turbolinksによって画面遷移は高速化しますが、次ページのHTML要素だけを読み取って表示するため、JavaScriptファイルが読み込まれず、JavaScriptにて設定したイベントが発火しないケースがあるため削除もしくはコメントアウト
 require("@rails/activestorage").start()
 require("channels")
-
+require('../preview') 
+// require('../preview') はapplication.jsから見たファイルの場所、ファイルを読み込むことができる
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
